3bd1baa7a90b05997596f061492fbbf6de9c17ef
sathvisath <143781899+sathvisath@users.noreply.github.com>
2025-03-05 11:30:34 +0530

    Add files via upload

---
+++ global_variables.py
@@ -0,0 +1,23 @@
+"""global variables are the variables that are declared outside the function .
+they can be used inside a function or outside also.
+"""
+x="awesome"
+
+def myfunc():
+    x="hello"
+    print("Python is "+x) #here the x refers to x="hello".
+myfunc() #calling a function
+
+print("Python is "+x) #here x refers to x="awesome".
+
+# we can also use a global variable inside a function
+def myfunc2():
+    print("I am "+x) #here x refers to the global variable x="awesome"
+myfunc2()
+
+# we can also declare a global variable inside a function using the 'GLOBAL' keyword
+def myfunc3():
+    global x
+    x="World" # we have to assign the value in the next line after declaring the variable is global
+    print("Hello "+x)
+myfunc3()
