ca27e583efa46cde6407f1f64b34298a40e9924c
sathvisath <143781899+sathvisath@users.noreply.github.com>
2025-03-20 11:01:39 +0530

    Add files via upload

---
+++ string3.py
@@ -0,0 +1,13 @@
+txt="banana"
+x=txt.center(12)
+print(x)          #output :  "   banana   "
+
+txt="banana"
+x=txt.center(12,'p')
+print(x)          #output : pppbananappp
+
+c=txt.count("banana")
+print(c)          #output : 1
+
+a=txt.zfill(7)     
+print(a)          #output : 0banana
